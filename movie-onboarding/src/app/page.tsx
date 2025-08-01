8e565203b55c1ab10fcc54b969ac96d2eab4a61f
Tubby101 <gckyoshi@gmail.com>
2025-08-01 10:28:08 -0400

    Reverting testing changes

--- movie-onboarding/src/app/page.tsx
+++ movie-onboarding/src/app/page.tsx
@@ -2,7 +2,7 @@ import { redirect } from "next/navigation";
 import { ModalButton } from "@/components/test";
 
 export default function Home() {
-  //redirect('/list/1');
+  redirect('/list/1');
   return (
     <div className="">
       <ModalButton />
